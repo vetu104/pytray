3223f32ccaa2bc1f376f7d4eddacae8ee9afd75c
Janne Veteläinen <janne.vetelainen@elisanet.fi>
2024-02-24 02:35:38 +0200

    Use GestureClick instead of MenuButton, because MenuButton only activates on left click

--- pytray.py
+++ pytray.py
@@ -31,12 +31,13 @@ else:
                         stream=sys.stderr, level="WARNING")
 
 script_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
+resources_dir = f"{script_dir}/Resources"
 
 
 # def load_css(app):
 #     provider = Gtk.CssProvider.new()
 #     provider.load_from_path(script_dir + "/Resources/style.css")
-#    
+#
 #     Gtk.StyleContext.add_provider_for_display(
 #         Gdk.Display.get_default(),
 #         provider,
@@ -95,188 +96,215 @@ def argb_to_rgba(icon_bytes):
     return arr
 
 
-def pixmap_to_image(pixmap):
-    width, height, argb = pixmap.unpack()[0]
-    rgba = argb_to_rgba(argb)
-
-    size = len(rgba)
-    padding = size / width - 4 * width
-    rowstride = 4 * width + padding
-
-    gbytes = GLib.Bytes.new(argb)
-    pixbuf = GdkPixbuf.Pixbuf.new_from_bytes(
-            gbytes,
-            GdkPixbuf.Colorspace.RGB,
-            True,
-            8,
-            width,
-            height,
-            rowstride)
-    texture = Gdk.Texture.new_for_pixbuf(pixbuf)
-
-    image = Gtk.Image.new_from_paintable(texture)
-
-    return (width, height, image)
-
-
-def find_icon(item, proxy):
-    icon = proxy.get_cached_property("IconName")
-    overlayicon = proxy.get_cached_property("OverlayIconName")
-    attentionicon = proxy.get_cached_property("AttentionIconName")
-    iconpix = proxy.get_cached_property("IconPixmap")
-    overlaypix = proxy.get_cached_property("OverlayIconPixmap")
-    attentionpix = proxy.get_cached_property("AttentionIconPixmap")
-    if icon:
-        sicon = icon.get_string()
-        logging.debug("Iconfinder returned icon")
-        image = Gtk.Image.new_from_icon_name(sicon)
-        return image
-    # elif attentionicon:
-    #     logging.debug("Iconfinder returned attentionicon")
-    #     return attentionicon
-    elif iconpix:
-        logging.debug("Iconfinder returned pixmap")
-        image = pixmap_to_image(iconpix)[2]
-
-        return image
-    # elif attentionpix:
-    #     logging.debug("Iconfinder returned attentionpixmap")
-    #     return attentionpix
-    else:
-        return item.icon
-        logging.warning(f"No icon found for {proxy.dbuspath}")
-
-
-def actionbuilder(menu, proxy):
-    actiongroup = Gio.SimpleActionGroup.new()
-    for item in menu:
-
-        def on_action(self, param, userdata=None):
-            proxy.call(
-                    "Event",
-                    GLib.Variant("(isvu)", (item[0], "clicked", GLib.Variant("s", ""), time.time())),
-                    Gio.DBusCallFlags.NONE,
-                    -1)
-
-        action = Gio.SimpleAction.new(str(item[0]))
-        action.connect("activate", on_action)
-        actiongroup.add_action(action)
-
-    return actiongroup
-
-
-def menubuilder(items, proxy):
-    menu = Gio.Menu.new()
-    for item in items:
-        menuitem = Gio.MenuItem.new(item[1], f"menuclick.{item[0]}")
-        menu.append_item(menuitem)
-    return menu
-
-
-class TrayItem(Gtk.MenuButton):
-    def __init__(self, dbusname, dbusobj):
+class TrayItem(Gtk.Image):
+    def __init__(self, busname, busobj):
         super().__init__()
-        self.busname = dbusname
-        self.menuproxy = None
-        print(self.get_child())
-        print(self.get_css_classes())
+        self._sniproxy = None
+        self._menuproxy = None
 
-        with open(script_dir + "/Resources/StatusNotifierItem.xml", "r") as f:
-            nodeinfo = Gio.DBusNodeInfo.new_for_xml(f.read())
-            Gio.DBusProxy.new_for_bus(
-                    Gio.BusType.SESSION,
-                    Gio.DBusProxyFlags.NONE,
-                    nodeinfo.interfaces[0],
-                    dbusname,
-                    dbusobj,
-                    "org.kde.StatusNotifierItem",
-                    None,
-                    self.on_proxy_ready)
+        self._menulayout = None
+        self._menumodel = Gio.Menu.new()
+        self._popovermenu = None
+        self.busname = busname
 
-    def on_menu_ready(self, obj, token):
-        proxy = obj.new_for_bus_finish(token)
-        ret = proxy.call_sync(
-                "GetLayout",
-                GLib.Variant("(iias)", (0, -1, [])),
-                Gio.DBusCallFlags.NONE,
-                -1)
-        layout = ret[1]  # (0, {'children-display': 'submenu'}, [actual menuitems])
-        menuitems = layout[2]  # [(id, {"label": "somelabel"}, []), (id, {"label": "somelabel"}, []), ...]
-        lst = []
-        for item in menuitems:
-            # item[2] == submenu ???
-            if "label" in item[1].keys():
-                lst.append((item[0], item[1]["label"]))
-        self.actions = actionbuilder(lst, proxy)
-        self.insert_action_group("menuclick", self.actions)
-        menumodel = menubuilder(lst, proxy)
-        popover = Gtk.PopoverMenu.new_from_model(menumodel)
-        popover.set_autohide(False)
-        popover.set_has_arrow(False)
-        self.set_popover(popover)
-        self.popover = popover
+        self._leftclick = Gtk.GestureClick.new()
+        self._leftclick.connect("pressed", self._on_leftclick)
+        self._leftclick.set_button(1)
+        self.add_controller(self._leftclick)
+        self._rightclick = Gtk.GestureClick.new()
+        self._rightclick.connect("pressed", self._on_rightclick)
+        self._rightclick.set_button(3)
+        self.add_controller(self._rightclick)
 
-    def on_proxy_ready(self, obj, token):
-        proxy = obj.new_for_bus_finish(token)
-        self.icon = find_icon(self, proxy)
-        self.set_child(self.icon)
-        self.menupath = proxy.get_cached_property("Menu").get_string()
-        if not self.menupath.startswith(("/org/ayatana/NotificationItem", "/MenuBar", "/com/canonical/dbusmenu")):
-            logging.warning(f"unknown dbusmenupath {self.menupath}")
-            return
-        with open(script_dir + "/Resources/DBusMenu.xml", "r") as f:
+        self.set_icon_size(Gtk.IconSize.NORMAL)
+
+        with open(f"{resources_dir}/StatusNotifierItem.xml", "r") as f:
             nodeinfo = Gio.DBusNodeInfo.new_for_xml(f.read())
             Gio.DBusProxy.new_for_bus(
                     Gio.BusType.SESSION,
                     Gio.DBusProxyFlags.NONE,
                     nodeinfo.interfaces[0],
                     self.busname,
-                    self.menupath,
+                    busobj,
+                    "org.kde.StatusNotifierItem",
+                    None,
+                    self._on_sniproxy_ready)
+
+    def _on_leftclick(self, obj, n_press, x, y):
+        self._sniproxy.call(
+                "Activate",
+                GLib.Variant("(ii)", [0, 0]),
+                Gio.DBusCallFlags.NONE,
+                -1)
+
+    def _on_rightclick(self, obj, n_press, x, y):
+        self._menuproxy.call(
+                "AboutToShow",
+                GLib.Variant("(i)", [0]),
+                Gio.DBusCallFlags.NONE,
+                -1,
+                None,
+                self._on_menushow)
+
+    def _on_menushow(self, obj, res):
+        try:
+            needUpdate = obj.call_finish(res).unpack()[0]
+            if needUpdate is True:
+                self._update_menu()
+        except gi.repository.GLib.GError:
+            pass
+
+        self._popovermenu.popup()
+
+    def _on_sniproxy_ready(self, obj, result):
+        self._sniproxy = obj.new_for_bus_finish(result)
+
+        self._find_icon()
+
+        self._sniproxy.connect("g-signal", self._on_snisignal)
+
+        known_menuobjects = (
+                "/MenuBar",
+                "/com/canonical/dbusmenu",
+                "/org/ayatana/NotificationItem")
+
+        invalid_menuobjects = ("/NO_DBUSMENU")
+
+        menubusobj = self._sniproxy.get_cached_property(
+                "Menu").get_string()
+
+        if menubusobj.startswith(invalid_menuobjects):
+            return
+        if not menubusobj.startswith(known_menuobjects):
+            logging.warning(f"Unknown Dbusmenu object: {self.menubusobj}")
+            return
+
+        with open(f"{resources_dir}/DBusMenu.xml", "r") as f:
+            nodeinfo = Gio.DBusNodeInfo.new_for_xml(f.read())
+            Gio.DBusProxy.new_for_bus(
+                    Gio.BusType.SESSION,
+                    Gio.DBusProxyFlags.NONE,
+                    nodeinfo.interfaces[0],
+                    self.busname,
+                    menubusobj,
                     "com.canonical.dbusmenu",
                     None,
-                    self.on_menu_ready)
-        proxy.connect("g-signal", self.on_signal)
-        self.proxy = proxy
+                    self._on_menuproxy_ready)
 
-    def on_signal(self, proxy, sender, signal, params, userdata=None):
+    def _find_icon(self):
+        iconname = self._sniproxy.get_cached_property("IconName")
+        # overlayicon = proxy.get_cached_property("OverlayIconName")
+        # attentionicon = proxy.get_cached_property("AttentionIconName")
+        iconpix = self._sniproxy.get_cached_property("IconPixmap")
+        # overlaypix = proxy.get_cached_property("OverlayIconPixmap")
+        # attentionpix = proxy.get_cached_property("AttentionIconPixmap")
+        if iconname:
+            self._iconname = iconname.get_string()
+            self._texture = None
+
+            self.set_from_icon_name(self._iconname)
+        # elif attentionicon:
+        #     logging.debug("Iconfinder returned attentionicon")
+        #     return attentionicon
+        elif iconpix:
+            width, height, argb = iconpix.unpack()[0]
+            rgba = argb_to_rgba(argb)
+
+            size = len(rgba)
+            padding = size / width - 4 * width
+            rowstride = 4 * width + padding
+
+            gbytes = GLib.Bytes.new(argb)
+            pixbuf = GdkPixbuf.Pixbuf.new_from_bytes(
+                    gbytes,
+                    GdkPixbuf.Colorspace.RGB,
+                    True,
+                    8,
+                    width,
+                    height,
+                    rowstride)
+
+            self._texture = Gdk.Texture.new_for_pixbuf(pixbuf)
+            self._iconname = None
+
+            self.set_from_paintable(self._texture)
+        # elif attentionpix:
+        #     logging.debug("Iconfinder returned attentionpixmap")
+        #     return attentionpix
+        else:
+            return
+
+    def _on_snisignal(self, proxy, sender, signal, params, userdata=None):
         if signal == "NewIcon":
-            self.icon = find_icon(self, proxy)
-            self.set_child(self.icon)
+            self._find_icon()
+
+    def _on_menuproxy_ready(self, obj, result):
+        self._menuproxy = obj.new_for_bus_finish(result)
+
+        self._menuproxy.call(
+                "GetLayout",
+                # parentid, depth(-1=all), props([]=all)
+                GLib.Variant("(iias)", (0, -1, [])),
+                Gio.DBusCallFlags.NONE,
+                -1,
+                None,
+                self._on_menulayout_ready)
+
+        self._menuproxy.connect("g-signal", self._on_menusignal)
+
+    def _on_menulayout_ready(self, obj, result):
+        self._menulayout = obj.call_finish(result)
+
+        self._update_menu()
+
+    def _on_menusignal(self, proxy, sender, signal, params):
+        if signal == "ItemsPropertiesUpdated" or signal == "LayoutUpdated":
+            self._update_menu()
+
+    def _update_menu(self):
+
+        self._popovermenu = None
+        self._actiongroup = None
+        self._popovermenu = Gtk.PopoverMenu()
+        self._popovermenu.set_has_arrow(False)
+        self._actiongroup = Gio.SimpleActionGroup.new()
+
+        self._menurevision, menuprops = self._menulayout
+        rootid, _, rootprops = menuprops
+        for item in rootprops:
+            if ("visible" in item[1].keys()) and (item[1]["visible"] is False):
+                continue
+
+            if "label" in item[1].keys():
+                self._menumodel.append(item[1]["label"], f"menuclick.{item[0]}")
+                action = self._createaction(item)
+                self._actiongroup.add_action(action)
+
+        self.insert_action_group("menuclick", self._actiongroup)
+        self._popovermenu.set_menu_model(self._menumodel)
+        self._popovermenu.set_parent(self)
+
+    def _createaction(self, item):
+        action = Gio.SimpleAction.new(str(item[0]))
+
+        def on_action(actionobj, param):
+            self._menuproxy.call(
+                    "Event",
+                    GLib.Variant("(isvu)", (item[0], "clicked", GLib.Variant("s", ""), time.time())),
+                    Gio.DBusCallFlags.NONE,
+                    -1)
+
+        action.connect("activate", on_action)
+
+        return action
 
 
 class StatusNotifierHost:
     def __init__(self):
-        self.proxy = None
-
-    def on_signal(self, proxy, sender, signal, params, userdata=None):
-        if signal == "StatusNotifierItemRegistered":
-            dbuspath = params[0].split("#")[0]
-            dbusobj = params[0].split("#")[1]
-            titem = TrayItem(dbuspath, dbusobj)
-            app.window.add_item(titem)
-
-        elif signal == "StatusNotifierItemUnregistered":
-            dbuspath = params[0].split("#")[0]
-            app.window.remove_item(dbuspath)
-
-    def on_proxy_acquired(self, conn, res):
-        proxy = conn.new_for_bus_finish(res)
-        proxy.call(
-                "RegisterStatusNotifierHost",
-                GLib.Variant.new_tuple(GLib.Variant.new_string("org.vetu104.Pytray")),
-                Gio.DBusCallFlags.NONE,
-                -1)
-        vnewitems = proxy.get_cached_property("RegisteredStatusNotifierItems")
-        newitems = vnewitems.unpack()
-        for item in newitems:
-            dbuspath, dbusobj = item.split("#")
-            titem = TrayItem(dbuspath, dbusobj)
-            app.window.add_item(titem)
-        proxy.connect("g-signal", self.on_signal)
-        self.proxy = proxy
+        self._snwproxy = None
 
     def start(self):
-        with open(script_dir + "/Resources/StatusNotifierWatcher.xml", "r") as f:
+        with open(f"{resources_dir}/StatusNotifierWatcher.xml", "r") as f:
             nodeinfo = Gio.DBusNodeInfo.new_for_xml(f.read())
             Gio.DBusProxy.new_for_bus(
                     Gio.BusType.SESSION,
@@ -286,7 +314,37 @@ class StatusNotifierHost:
                     "/StatusNotifierWatcher",
                     "org.kde.StatusNotifierWatcher",
                     None,
-                    self.on_proxy_acquired)
+                    self._on_proxy_acquired)
+
+    def _on_proxy_acquired(self, conn, res):
+        self._snwproxy = conn.new_for_bus_finish(res)
+
+        self._snwproxy.call(
+                "RegisterStatusNotifierHost",
+                GLib.Variant("(s)", ["org.vetu104.Pytray"]),
+                Gio.DBusCallFlags.NONE,
+                -1)
+
+        self._snwproxy.connect("g-signal", self._on_signal)
+
+        initial_items = self._snwproxy.get_cached_property(
+                "RegisteredStatusNotifierItems").unpack()
+
+        for item in initial_items:
+            busname, busobj = item.split("#")
+            trayitem = TrayItem(busname, busobj)
+            app.window.add_item(trayitem)
+
+    def _on_signal(self, proxy, sender, signal, params, userdata=None):
+        if signal == "StatusNotifierItemRegistered":
+            busname = params[0].split("#")[0]
+            busobj = params[0].split("#")[1]
+            trayitem = TrayItem(busname, busobj)
+            app.window.add_item(trayitem)
+
+        elif signal == "StatusNotifierItemUnregistered":
+            busname = params[0].split("#")[0]
+            app.window.remove_item(busname)
 
 
 if __name__ == "__main__":
